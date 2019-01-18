1b294cb391d198e122780d569bd5f1f97ef67f97
Pedro <phb2905gmail.com>
2019-01-18 10:09:07 -0200

    Upload1

---
+++ src/main/java/br/com/company/App.java
@@ -0,0 +1,13 @@
+package br.com.company;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/br/com/company/Personagem.java
@@ -0,0 +1,14 @@
+package br.com.App;
+
+public class Personagem(){
+	private float F, A, E, V, D;
+
+	public Personagem(float F, float, A, float E){
+		this.V = F + 2*E - 1.5*A;
+		this.D = 0.5*F + 2*A - 0.5*E;
+	}
+
+	public Ataque(float F){
+	
+	} 
+}
